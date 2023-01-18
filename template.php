16e8a0ea1633e71bf4fee0b76de9a10d941a6a18
Pankaj Joshi <pnkjjoshi736@gmail.com>
2023-01-19 00:27:38 +0530

    template.php
    Code for template file

---
+++ template.php
@@ -0,0 +1,34 @@
+<div class="page-case-studies common-padding">
+        <div class="container">
+            <div class="row" id="case-studies">
+                <?php
+                $args = [
+                    'post_type' => 'post_case_studies',
+                    'post_status' => 'publish',
+                    'posts_per_page' => '4',
+                    'orderby' => 'date',
+                    'order' => 'DESC',
+                ];
+                $loop = new WP_Query($args);
+                $found_post = $loop->found_posts;
+                $i = 1;
+                if ($loop->have_posts()) {
+                    while ($loop->have_posts()) {
+                        $loop->the_post();
+                        ?>
+                        <?php get_template_part('inc/Case-Studies/case', 'loop'); ?>
+                        <?php
+                    }
+                    wp_reset_postdata();
+                    $i++;
+                }
+                ?>
+            </div>
+
+            <?php if ($loop->max_num_pages > 1): ?>
+                <div id="load-more-posts">
+                    <span class="theme-btn">Load More</span>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
